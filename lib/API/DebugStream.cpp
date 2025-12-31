416290b075675424c337bd1365c9f9f04eaac1e8
Chris B <cbieneman@microsoft.com>
2025-12-31 13:41:29 -0600

    clang-tidy

--- lib/API/DebugStream.cpp
+++ lib/API/DebugStream.cpp
@@ -33,26 +33,26 @@ llvm::Error offloadtest::PrintDebugStream(const Buffer &Buffer,
                                    "No string table part found in DXContainer");
   const uint32_t *const Start =
       reinterpret_cast<const uint32_t *>(Buffer.Data[0].get());
-  size_t NumEntries = Buffer.Size / sizeof(uint32_t);
+  size_t const NumEntries = Buffer.Size / sizeof(uint32_t);
   const uint32_t *Data = Start;
   const uint32_t *const End = Start + NumEntries;
 
   // Must be able to read at least the wave count, string offset, and arg size!
   while (Data + 3 < End) {
-    uint32_t WaveCount = *Data++;
+    uint32_t const WaveCount = *Data++;
     if (WaveCount == 0)
       break;
-    uint32_t StrOffset = *Data++;
-    uint32_t ArgSize = *Data++;
+    uint32_t const StrOffset = *Data++;
+    uint32_t const ArgSize = *Data++;
 
-    llvm::StringRef Str =
+    llvm::StringRef const Str =
         StringTable.slice(StrOffset, StringTable.find('\0', StrOffset));
     if (Data + (WaveCount * (ArgSize / sizeof(uint32_t))) > End)
       return llvm::createStringError(
           std::errc::invalid_argument,
           "Debug stream data goes out of bounds of the buffer");
     llvm::SmallString<512> WorkingStr;
-    for (uint32_t i = 0; i < WaveCount; ++i) {
+    for (uint32_t I = 0; I < WaveCount; ++I) {
       llvm::raw_svector_ostream OS(WorkingStr);
       const uint32_t *const ArgEnd = Data + (ArgSize / sizeof(uint32_t));
       for (size_t S = 0; S < Str.size(); ++S) {
@@ -69,22 +69,22 @@ llvm::Error offloadtest::PrintDebugStream(const Buffer &Buffer,
           }
           switch (Str[S]) {
           case 'd': {
-            int32_t Val = *reinterpret_cast<const int32_t *>(Data++);
+            int32_t const Val = *reinterpret_cast<const int32_t *>(Data++);
             OS << Val;
             break;
           }
           case 'u': {
-            uint32_t Val = *Data++;
+            uint32_t const Val = *Data++;
             OS << Val;
             break;
           }
           case 'f': {
-            float Val = *reinterpret_cast<const float *>(Data++);
+            float const Val = *reinterpret_cast<const float *>(Data++);
             OS << Val;
             break;
           }
           case 'x': {
-            uint32_t Val = *Data++;
+            uint32_t const Val = *Data++;
             OS << llvm::format_hex(Val, 8, /*Upper=*/false);
             break;
           }
